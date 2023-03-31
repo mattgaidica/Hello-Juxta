f7fa08690202281d320644cfe49dab112e0b2cb9
Matt Gaidica, Ph.D <matt@gaidi.ca>
2023-03-31 12:35:45 -0400

    Timezone update (GMT -> ET)

--- Juxta.R
+++ Juxta.R
@@ -5,6 +5,9 @@ library(tidyr)
 library(dplyr)
 library(lubridate)
 
+# Set option to display warnings in the browser console
+options(shiny.sanitize.errors = FALSE)
+
 ui <- fluidPage(
   tags$head(
     tags$style(HTML("
@@ -173,11 +176,11 @@ server <- function(input, output, session) {
   })
   
   # META
-  output$myText <- renderText({
-    "This is my text."
-  })
+  # output$myText <- renderText({
+  #   "This is my text."
+  # })
   
-  meta_input_data <- reactive({
+  observeEvent(input$meta_input_text, {
     req(input$meta_input_text)
     input_text <- input$meta_input_text
     input_text <- gsub("\r", "", input_text)
@@ -187,74 +190,78 @@ server <- function(input, output, session) {
     input_text <- strsplit(input_text, ",")
     input_df <- data.frame(do.call(rbind, input_text))
     colnames(input_df) <- c("subject", "data_type", "data_value", "local_time")
-    input_df$local_time <- as.POSIXct(input_df$local_time, format = "%s", origin = "1970-01-01")
-    input_df
-  })
-  
-  output$deg_c_plot <- renderPlot({
-    # Subset the input data to only include "deg_c" data type
-    subset_data <- meta_input_data() %>%
-      filter(data_type == "deg_c")
+    # input_df$local_time <- as.POSIXct(input_df$local_time, format = "%s", origin = "1970-01-01", tzone = "ET")
+    input_df$local_time <- with_tz(as.POSIXct(input_df$local_time, format = "%s", origin = "1970-01-01"), tzone = "GMT") %>%
+      with_tz(tzone = "America/New_York")
     
-    # Convert local_time to a datetime format
-    subset_data$local_time <- as.POSIXct(subset_data$local_time, origin = "1970-01-01")
+    output$deg_c_plot <- renderPlot({
+      # Subset the input data to only include "deg_c" data type
+      subset_data <- input_df %>%
+        filter(data_type == "deg_c")
+      
+      # Convert local_time to a datetime format
+      subset_data$local_time <- as.POSIXct(subset_data$local_time, origin = "1970-01-01")
+      
+      # Create the plot for deg_c
+      ggplot(subset_data, aes(x = local_time, y = as.numeric(data_value))) +
+        geom_line(color = "blue") +
+        ylab("deg_c") +
+        scale_x_datetime(date_breaks = "1 hour", date_labels = "%m/%d %H:%M") +
+        scale_y_continuous(limits = c(15, 35), expand = expand_scale(mult = c(0, 0.1))) +
+        theme_minimal() +
+        theme(axis.title.x = element_blank(),
+              axis.title.y = element_text(color = "blue"),
+              axis.text.y = element_text(color = "blue")) +
+        theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+    })
     
-    # Create the plot for deg_c
-    ggplot(subset_data, aes(x = local_time, y = as.numeric(data_value))) +
-      geom_line(color = "blue") +
-      ylab("deg_c") +
-      scale_x_datetime(date_breaks = "1 hour", date_labels = "%m/%d %H:%M") +
-      scale_y_continuous(limits = c(15, 30), expand = expand_scale(mult = c(0, 0.1))) +
-      theme_minimal() +
-      theme(axis.title.x = element_blank(),
-            axis.title.y = element_text(color = "blue"),
-            axis.text.y = element_text(color = "blue")) +
-      theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-  })
-  
-  output$vbatt_plot <- renderPlot({
-    # Subset the input data to only include "vbatt" data type
-    subset_data <- meta_input_data() %>%
-      filter(data_type == "vbatt")
+    output$vbatt_plot <- renderPlot({
+      # Subset the input data to only include "vbatt" data type
+      subset_data <- input_df %>%
+        filter(data_type == "vbatt")
+      
+      # Convert local_time to a datetime format
+      subset_data$local_time <- as.POSIXct(subset_data$local_time, origin = "1970-01-01")
+      
+      # Create the plot for vbatt
+      ggplot(subset_data, aes(x = local_time, y = as.numeric(data_value))) +
+        geom_line(color = "red") +
+        ylab("vbatt") +
+        scale_x_datetime(date_breaks = "1 hour", date_labels = "%m/%d %H:%M") +
+        scale_y_continuous(limits = c(2, 5), expand = expand_scale(mult = c(0, 0.1))) +
+        theme_minimal() +
+        theme(axis.title.x = element_blank(),
+              axis.title.y = element_text(color = "red"),
+              axis.text.y = element_text(color = "red")) +
+        theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+      
+    })
     
-    # Convert local_time to a datetime format
-    subset_data$local_time <- as.POSIXct(subset_data$local_time, origin = "1970-01-01")
-    
-    # Create the plot for vbatt
-    ggplot(subset_data, aes(x = local_time, y = as.numeric(data_value))) +
-      geom_line(color = "red") +
-      ylab("vbatt") +
-      scale_x_datetime(date_breaks = "1 hour", date_labels = "%m/%d %H:%M") +
-      scale_y_continuous(limits = c(2, 5), expand = expand_scale(mult = c(0, 0.1))) +
-      theme_minimal() +
-      theme(axis.title.x = element_blank(),
-            axis.title.y = element_text(color = "red"),
-            axis.text.y = element_text(color = "red")) +
-      theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-    
-  })
-  
-  output$xl_hist <- renderPlot({
-    # Subset the input data to only include "xl" data type
-    subset_data <- meta_input_data() %>%
-      filter(data_type == "xl")
-    
-    # Convert local_time to a datetime format
-    subset_data$local_time <- as.POSIXct(subset_data$local_time, origin = "1970-01-01")
-    
-    # Extract hour of day from local_time
-    subset_data$hour_of_day <- hour(subset_data$local_time)
-    
-    # Create a histogram of the number of "xl" occurrences by hour
-    ggplot(subset_data, aes(x = hour_of_day, fill = as.factor(hour_of_day))) +
-      geom_histogram() +
-      xlab("Hour of Day") +
-      ylab("Number of Occurrences") +
-      scale_fill_discrete(name = "Hour of Day", labels = function(x) format(strptime(x, format = "%H"), "%I %p")) +
-      scale_x_continuous(breaks = 0:23, limits = c(0, 23)) +
-      theme_minimal() +
-      guides(fill = FALSE) +
-      theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+    output$xl_hist <- renderPlot({
+      # Subset the input data to only include "xl" data type
+      subset_data <- input_df %>%
+        filter(data_type == "xl")
+      
+      # Filter out rows with missing local_time values
+      subset_data <- subset_data %>% filter(!is.na(local_time))
+      
+      # Convert local_time to a datetime format
+      subset_data$local_time <- as.POSIXct(subset_data$local_time, origin = "1970-01-01")
+      
+      # Extract hour of day from local_time
+      subset_data$hour_of_day <- hour(subset_data$local_time)
+      
+      # Create a histogram of the number of "xl" occurrences by hour
+      ggplot(subset_data, aes(x = hour_of_day, fill = as.factor(hour_of_day))) +
+        geom_histogram(binwidth = 1) +
+        xlab("Hour of Day") +
+        ylab("Number of Occurrences") +
+        scale_fill_discrete(name = "Hour of Day", labels = function(x) format(strptime(x, format = "%H"), "%I %p")) +
+        scale_x_continuous(breaks = c(0, 6, 12, 18, 23), limits = c(0, 23)) +
+        theme_minimal() +
+        guides(fill = FALSE) +
+        theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+    })
   })
   
 }
